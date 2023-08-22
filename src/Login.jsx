f1180f0f7d2fc9e5335e7db27515a4155abb0abd
EugeneSim <jcseugene@gmail.com>
2023-08-22 11:47:26 +0800

    version 2 update

---
+++ src/Login.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import IrvinLogo from './images/irvins-logo.png'
+export const Login = (props) => {
+  const [user, setUser] = useState("");
+  const [pass, setPass] = useState("");
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log(user);
+  // };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Simulate a login check here (you can replace this with your actual login logic)
+    const registeredUser = JSON.parse(localStorage.getItem("registeredUser"));
+    if (registeredUser && user === registeredUser.user && pass === registeredUser.password) {
+      // Successful login, switch to the Profile page
+      props.onFormSwitch("profile"); //need to edit this to onformswitch to lead into Profile
+    } else {
+      window.alert("Login Failed");
+      console.log("Login failed"); // Handle failed login
+    }
+  };
+
+  return (
+    <div className="auth-form-container">
+    <img src={IrvinLogo} alt="logo" style ={{width:'250px',}} /> 
+    <h1>Welcome to Irvins' <br></br> Internet Banking</h1>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <label htmlFor="user">Username</label>
+        <input
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+          type="user"
+          placeholder="Enter your username"
+          id="user"
+          name="user"
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          value={pass}
+          onChange={(e) => setPass(e.target.value)}
+          type="password"
+          placeholder="Enter your password"
+          id="password"
+          name="password"
+        />
+        <button type="submit">Login</button>
+      </form>
+      <button className= "link-btn" onClick={() => props.onFormSwitch("register")}>
+        Don't have an account? Register here!
+      </button>
+    </div>
+  );
+};
---
+++ src/RegisterCollab.js
@@ -0,0 +1,163 @@
+import React, { useState } from "react";
+
+function RegistrationForm(props) {
+  const [formData, setFormData] = useState({
+    accountnumber: "",
+    email: "",
+    username: "",
+    firstname: "",
+    lastname: "",
+    address: "",
+    phonenumber: "",
+    nric: "",
+    dob: "",
+    country: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    // Simulate registration process by storing data in local storage
+    localStorage.setItem("registeredUser", JSON.stringify(formData));
+    window.alert("Successful Registration!");
+    // After successful registration, switch to the login form
+    props.onFormSwitch("login");
+  };
+
+  return (
+    <div className="registration-form">
+  <div className="logo-container">
+    <img
+      src={require("./images/irvins-logo.png")}
+      alt="Logo"
+      className="logo"
+    />
+  </div>
+      <h2>Irvins' Internet Banking <br></br> Registration Page</h2>
+      <form onSubmit={handleSubmit}>
+      <label>Account Number:</label>
+        <input
+          type="accountnumber"
+          name="accountnumber"
+          value={formData.accountnumber}
+          onChange={handleChange}
+          required
+        />
+
+        <label>Email:</label>
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+
+        <label>Username:</label>
+        <input
+          type="user"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+          required
+        />
+
+        <label>First Name:</label>
+        <input
+          type="text"
+          name="firstname"
+          value={formData.firstname}
+          onChange={handleChange}
+          required
+        />
+
+        <label>Last Name:</label>
+        <input
+          type="text"
+          name="lastname"
+          value={formData.lastname}
+          onChange={handleChange}
+          required
+        />
+
+        <label>Address:</label>
+        <input
+          type="text"
+          name="address"
+          value={formData.address}
+          onChange={handleChange}
+          required
+        />
+
+        <label>Phone Number:</label>
+        <input
+          type="tel"
+          name="phonenumber"
+          value={formData.phonenumber}
+          onChange={handleChange}
+          required
+        />
+
+        <label>NRIC:</label>
+        <input
+          type="text"
+          name="nric"
+          value={formData.nric}
+          onChange={handleChange}
+          required
+        />
+
+        <label>Date of Birth:</label>
+        <input
+          type="date"
+          name="dob"
+          value={formData.dob}
+          onChange={handleChange}
+          required
+        />
+
+        <label>Country:</label>
+        <input
+          type="text"
+          name="country"
+          value={formData.country}
+          onChange={handleChange}
+          required
+        />
+
+        <label>Password:</label>
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+
+        <label>Confirm Password:</label>
+        <input
+          type="password"
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          onChange={handleChange}
+          required
+        />
+
+        <button type="submit">Register</button>
+      </form>
+      <button className= "link-btn" onClick={()=>props.onFormSwitch('login')}>Already have an account? Login here!</button>
+    </div>
+  );
+}
+
+export default RegistrationForm;
